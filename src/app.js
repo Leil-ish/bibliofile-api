96a8b2f43d7dacffd47836941ff0fa35c772a6a9
Leila Anderson <leilacanderson@gmail.com>
2019-09-08 19:50:55 -0500

    added error logging

--- src/app.js
+++ src/app.js
@@ -33,6 +33,7 @@ app.get('/', (req, res) => {
 
 
 app.use(function errorHandler(error, req, res, next) {
+    console.log(error)
     let response
     if (NODE_ENV === 'production') {
       response = { error: 'Server error' }
