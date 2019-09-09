d53a5d0b82b63669e3e178ac3d1a665e52aac56e
Leila Anderson <leilacanderson@gmail.com>
2019-09-08 20:45:03 -0500

    updated postgres credentials

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DB_URL || 'postgresql://bibliofile@localhost/bibliofile',
+    DB_URL: process.env.DATABASE_URL || 'postgresql://bibliofile@localhost/bibliofile',
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
     //CLIENT_ORIGIN: 'https://leil-ish-bibliofile-app.now.sh/' || 'localhost:3000',
   }
