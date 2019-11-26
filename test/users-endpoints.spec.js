8057a0641d931039d0ebb3bce0d94f14cdc08782
Leila Anderson <leilacanderson@gmail.com>
2019-11-25 20:47:26 -0600

    bug fix in tests

--- test/users-endpoints.spec.js
+++ test/users-endpoints.spec.js
@@ -6,7 +6,7 @@ const helpers = require('./test-helpers')
 describe('Users Endpoints', function() {
   let db
 
-  const { testUsers } = helpers.makeArticlesFixtures()
+  const { testUsers } = helpers.makeBooksFixtures()
   const testUser = testUsers[0]
 
   before('make knex instance', () => {
